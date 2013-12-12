ba6e8d91c2a7de96fe5e46e7a293f2baea9a4dd3
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-12-12 14:25:08 -0600

    Add and Get List Item

---
+++ lib/linked_list.rb
@@ -0,0 +1,30 @@
+
+class LinkedList
+
+  def initialize
+    @head = nil
+  end
+
+  def add_item payload
+    if @head == nil
+      @head = LinkedListItem.new(payload)
+      node = @head
+    else
+      node = @head
+      while node.next_list_item != nil do
+        node = node.next_list_item
+      end
+      node.next_list_item = LinkedListItem.new(payload)
+    end
+  end
+
+  def get index
+    node = @head
+    return @head.payload if index == 0
+    for i in 0...index
+      node = node.next_list_item
+    end
+    return node.payload
+  end
+
+end
